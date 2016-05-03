6298ee07f1f92eb0748ad434f39e4129e37159df
shuheitsuji <shuheitsuji@icloud.com>
2016-05-03 23:02:57 +0900

    create user function about my page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  get   'users/:id'   =>  'users#show'
+  resources :users, only: [:show]
   resources :prototypes, only: [:index, :new, :show]
 end
 
