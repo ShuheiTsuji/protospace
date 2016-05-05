b4fb1eeb7658e47b67e610488d1a568aa989a1ca
shuheitsuji <shuheitsuji@icloud.com>
2016-05-05 09:35:20 +0900

    delete @user instance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
+  
   def update
     current_user.update(update_params)
     redirect_to user_path(current_user), notice: 'ログインに成功しました'
