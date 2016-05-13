678085d257e473503ab9c974b4b6ccebc6208724
shuheitsuji <shuheitsuji@icloud.com>
2016-05-13 23:16:27 +0900

    correct error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< HEAD
   root       'products#index'
   resources  :users   , only: [:show, :edit, :update] do
   resources  :products, only: [:new, :create, :index]
   end
+=======
+  root 'products#index'
+  resources :users , only: [:show, :edit, :update]
+  resources :products, only: [:new, :create, :index]
+>>>>>>> parent of 54e3e5f... create controller function and index.html.haml
 end
 
