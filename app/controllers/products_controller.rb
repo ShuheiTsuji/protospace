e581073aa09da96e03d11647ba48a4f8730cecf6
shuheitsuji <shuheitsuji@icloud.com>
2016-05-15 17:17:55 +0900

    create delete function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,7 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     if product.user_id == current_user.id
       product.destroy
+      redirect_to root_path , notice: 'succeed in delete'
     end
   end
 
