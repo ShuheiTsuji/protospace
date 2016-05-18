7e07646ea43a737207ac84e5697c5a34e6ec17ef
shuheitsuji <shuheitsuji@icloud.com>
2016-05-19 02:43:28 +0900

    define product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,8 @@ class Product < ActiveRecord::Base
   has_many   :likes, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :catch_copy, :concept, :title,  presence: true
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
 end
 
