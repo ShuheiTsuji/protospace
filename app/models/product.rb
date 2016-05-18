979267ae9c3df60314f26975c62434e1aa35b212
shuheitsuji <shuheitsuji@icloud.com>
2016-05-18 21:44:23 +0900

    set associatio

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many   :images
+  has_many   :likes, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :catch_copy, :concept, :title,  presence: true
 end
