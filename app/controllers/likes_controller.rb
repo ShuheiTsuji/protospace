0b4314da31839cfaad99944ac25866b13a867abe
shuheitsuji <shuheitsuji@icloud.com>
2016-05-21 16:33:09 +0900

    set @like @product @user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,16 @@ class LikesController < ApplicationController
   def create
     @like     = Like.create(user_id: current_user.id,  product_id: params[:product_id])
     @likes    = Like.where(product_id: params[:product_id])
-    @product  = Product.all
+    @product  = Product.find(params[:product_id])
+    @user     = User.find(params[:user_id])
   end
 
   def destroy
-    @like      = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
+    @like      = current_user.likes.find_by(product_id: params[:product_id])
     @like.destroy
     @likes    = Like.where(product_id: params[:product_id])
-    @product  = Product.all
+    @product  = Product.find(params[:product_id])
+    @user     = User.find(params[:user_id])
   end
 end
 
