94ee3c2543698e13d6842341a5d6b265cf1c40ba
shuheitsuji <shuheitsuji@icloud.com>
2016-05-21 18:16:27 +0900

    divide controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,24 @@
 class LikesController < ApplicationController
+  before_action :set_user, :set_product, :set_likes
   def create
     @like     = Like.create(user_id: current_user.id,  product_id: params[:product_id])
-    @likes    = Like.where(product_id: params[:product_id])
-    @product  = Product.find(params[:product_id])
-    @user     = User.find(params[:user_id])
   end
 
   def destroy
-    @like      = current_user.likes.find_by(product_id: params[:product_id])
+    @like     = current_user.likes.find_by(product_id: params[:product_id])
     @like.destroy
-    @likes    = Like.where(product_id: params[:product_id])
-    @product  = Product.find(params[:product_id])
+  end
+
+  def set_user
     @user     = User.find(params[:user_id])
+  end
+
+  def set_product
+    @product  = Product.find(params[:product_id])
+  end
+
+  def set_likes
+    @likes    = Like.where(product_id: params[:product_id])
   end
 end
 
