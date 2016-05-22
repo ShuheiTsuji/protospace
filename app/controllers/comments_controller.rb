e18e271db76b97a9e4dc17008c84430db6fb4f4e
shuheitsuji <shuheitsuji@icloud.com>
2016-05-22 23:52:45 +0900

    delete @comment and merge

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def create
     current_user.comments.create(comment_params)
     @product  = Product.find(params[:product_id])
-    @comment  = Comment.where(product_id: params[:product_id], user_id: params[:user_id])
     @user     = User.find(params[:user_id])
     @comments = @product.comments.includes(:user)
   end
@@ -10,7 +9,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(product_id: params[:product_id], user_id: current_user.id)
+    params.require(:comment).permit(:text).merge(product_id: params[:product_id])
   end
 end
 
