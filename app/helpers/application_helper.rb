ea82ca6793c09fd7223ef9801c32dee1bd325185
shuheitsuji <shuheitsuji@icloud.com>
2016-06-14 18:36:00 +0900

    set helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
+  def main_image
+    @product.images.main.present? ? image_tag("#{@product.images.main.first.image}") : ''
+  end
+
+  def sub_image
+    @product.images.main.present? ? @product.images.main : ''
+  end
 end
 
