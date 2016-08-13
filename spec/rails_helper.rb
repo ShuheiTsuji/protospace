efe90a1a8f2e3533663cfa37e7fa0218ae28bf26
shuheitsuji <shuheitsuji@icloud.com>
2016-08-14 01:01:57 +0900

    controller test require support/controller_macros

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'capybara/poltergeist'
+require 'support/controller_macros'
 Capybara.javascript_driver = :poltergeist
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -57,6 +59,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::TestHelpers, :type => :controller
+  config.include ControllerMacros, :type => :controller
 end
 
 
